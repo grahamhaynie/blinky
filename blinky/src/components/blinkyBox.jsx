ea9c5f70e38245b8f3e7df02a67b5c983659e11b
Graham Haynie <jgrahamh@vt.edu>
2020-11-15 14:00:36 -0500

    broke some stuff fixed some stuff who knows

--- blinky/src/components/blinkyBox.jsx
+++ blinky/src/components/blinkyBox.jsx
@@ -27,14 +27,14 @@ export default class BoxForm extends React.Component {
         this.state = {
             text: prompt,
             keyArray: [],
-            selectTrigger: false,
+            changeTrigger: false,
             selected: "",
             lineIndex: 0,
-            index: prompt.length,
         };
     }
 
     handleTextChange(e) {
+
         if(this.state.keyArray['Enter'] === 1){ 
             var command = this.state.text.slice(this.state.text.lastIndexOf('>') + 1);
             console.log('command: ' + command);
@@ -44,23 +44,26 @@ export default class BoxForm extends React.Component {
             this.setState({
                 text: newText,
                 lineIndex: 0,
-                index: this.state.index + prompt.length + 1,
+                changeTrigger: false,
             });
         }else if(this.state.keyArray['Backspace'] === 1){ 
+            
             // disable highlight select and delete, and deletion of anything before prompt
             if(this.state.selected === "" && this.state.lineIndex - 1 >= 0){
                 this.setState({
                     text: e.target.value,
                     lineIndex: this.state.lineIndex - 1,
-                    index: this.state.index - 1,
+                    changeTrigger: false,
                 });
             }
         }
-        else {
+
+        else if(this.state.changeTrigger){
+
             this.setState({
                 text: e.target.value,
                 lineIndex: this.state.lineIndex + 1,
-                index: this.state.index + 1,
+                changeTrigger: false,
             });
         }
 
@@ -72,40 +75,59 @@ export default class BoxForm extends React.Component {
 
     handleKeyDown(e){
 
+        // TODO - make one liner
+        var keys = this.state.keyArray;
+        keys[e.key] = 1;
+        this.setState({keyArray: keys});
+
+
         // disable up and down keys for now
         if(e.key === 'ArrowUp' || e.key == 'ArrowDown'){
             e.preventDefault();
             // TODO - add scroll through commands
         }else if(e.key === 'ArrowLeft'){
 
-            // TODO make so can't scroll left past prompt
+            if (this.state.lineIndex - 1 > 0){
+                this.setState({
+                    lineIndex: this.state.lineIndex - 1,
+                });
+            }else{
+                this.boxRef.current.selectionStart = e.target.selectionStart + 1;
+            }
+            //console.log(this.boxRef.current.textLength); 
+        }else if(e.key === 'ArrowRight'){
+           this.setState({
+                lineIndex: this.state.lineIndex + 1,
+            });
             
-            console.error("TODO");
-
+        }else{
+            this.setState({changeTrigger: true});
         }
 
-        // TODO - make one liner
-        var keys = this.state.keyArray;
-        keys[e.key] = 1;
-        this.setState({keyArray: keys});
     }
 
     handleKeyUp(e){
+
         var keys = this.state.keyArray;
         keys[e.key] = 0;
         this.setState({keyArray: keys});
     }
 
     handleClick(e){
-        this.boxRef.current.selectionStart = this.boxRef.current.selectionEnd = this.state.index;
+        this.boxRef.current.selectionStart = this.boxRef.current.selectionEnd = this.boxRef.current.textLength;
 
         // TODO - make less janky
 
     }
 
+    componentDidMount(){
+        this.boxRef.current.selectionStart = getPrompt().length + 1;
+    }
+
 
     // TODO - var vs let
-    // TODO  - make so user can't highlight ?
+    // TODO - fix kso not janky if spam keys
+    // TODO  - make so user can't highlight anything at all - bug: highlight text, pressing key deletes text
 
     render(){
         return(
